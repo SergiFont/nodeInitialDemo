2aa4d2eccbe0c0b5b22952b51146683edb42a902
SergiFont <sfontsmx2010@gmail.com>
2023-02-22 13:06:21 +0100

    entrega4.1 nivel1 completado

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/user', (req, res) => {
+    const response = {
+        name: 'Sergi',
+        edat: '29',
+        url: req.protocol + '://' + req.get('host') + req.url
+        }
+    res.json(response)
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
